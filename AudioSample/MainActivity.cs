a160ec69a95661da26f5f47e27057751e481d9f0
SaitouRedEyes <zeca.saitou@gmail.com>
2018-10-18 23:13:07 -0300

    Audio on Xamarin Android
    This is a simple project where we can play an audio built-in app, sdcard or online stream.

---
+++ AudioSample/MainActivity.cs
@@ -0,0 +1,114 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using Java.Lang;
+using Android.Util;
+
+namespace AudioSample
+{
+    [Activity(Label = "AudioSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private MediaPlayer mpRaw;
+        private MediaPlayer mpPath;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mpRaw = MediaPlayer.Create(this, Resource.Raw.intro);
+            mpPath = new MediaPlayer();
+
+            Button buttonPlayRaw = (Button)FindViewById(Resource.Id.buttonPlayRaw);
+            Button buttonPlayPath = (Button)FindViewById(Resource.Id.buttonPlayPath);
+            Button buttonStop = (Button)FindViewById(Resource.Id.buttonStop);
+
+            buttonPlayRaw.Click += ButtonPlayRaw_Click;
+            buttonPlayPath.Click += ButtonPlayPath_Click;
+            buttonStop.Click += ButtonStop_Click;
+        }
+
+        private void ButtonStop_Click(object sender, System.EventArgs e)
+        {
+            MPManager(1);
+        }
+
+        private void ButtonPlayPath_Click(object sender, System.EventArgs e)
+        {
+            if(mpPath != null && !mpPath.IsPlaying && !mpRaw.IsPlaying)
+            {
+                
+                try
+                {
+                    mpPath.Reset();
+                    mpPath.SetDataSource("http://soundbible.com/grab.php?id=2207&type=wav");
+                    //mpPath.SetDataSource("/sdcard/intro.mp3");
+                    mpPath.Prepare();
+                    mpPath.Start();
+                }
+                catch(Exception error)
+                {
+                    Log.Debug("ERRO", error.Message);
+                }
+            }
+        }
+
+        private void ButtonPlayRaw_Click(object sender, System.EventArgs e)
+        {
+            if (mpRaw != null && !mpRaw.IsPlaying && !mpPath.IsPlaying)
+            {
+                mpRaw = MediaPlayer.Create(this, Resource.Raw.intro);
+                mpRaw.Start();
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            MPManager(0);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            MPManager(1);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            MPManager(2);
+        }
+
+        private void MPManager(int state)
+        {
+            if (mpRaw != null && mpRaw.IsPlaying)
+            {
+                switch(state)
+                {
+                    case 0: mpRaw.Pause(); break;
+                    case 1: mpRaw.Stop(); break;
+                    case 2: mpRaw.Release(); break;
+                }
+                
+            }
+            else if (mpPath != null && mpPath.IsPlaying)
+            {
+                switch (state)
+                {
+                    case 0: mpPath.Pause(); break;
+                    case 1: mpPath.Stop(); break;
+                    case 2: mpPath.Release(); break;
+                }
+            }
+        }
+    }
+}
+
---
+++ AudioSample/obj/Debug/android/src/md541fe9f0777da919337c1a4471a907f36/MainActivity.java
@@ -0,0 +1,74 @@
+package md541fe9f0777da919337c1a4471a907f36;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onStop:()V:GetOnStopHandler\n" +
+			"n_onDestroy:()V:GetOnDestroyHandler\n" +
+			"";
+		mono.android.Runtime.register ("AudioSample.MainActivity, AudioSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AudioSample.MainActivity, AudioSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onStop ()
+	{
+		n_onStop ();
+	}
+
+	private native void n_onStop ();
+
+
+	public void onDestroy ()
+	{
+		n_onDestroy ();
+	}
+
+	private native void n_onDestroy ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
